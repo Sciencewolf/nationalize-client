beaa97ceb5e2114a164f8bf01cb3f0d52b3fa0bb
Marton Aron <aron17marton@gmail.com>
2024-02-29 12:26:08 +0100

    Initial commit

--- src/main/java/nationalize/Nationality.java
+++ src/main/java/nationalize/Nationality.java
@@ -4,7 +4,6 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
-@lombok.Data
 public class Nationality {
 
     private long count;
@@ -13,8 +12,36 @@ public class Nationality {
     @SerializedName("country")
     private List<Country> countries;
 
+    public Nationality(long count, String name, List<Country> countries) {
+        this.count = count;
+        this.name = name;
+        this.countries = countries;
+    }
 
-    @lombok.Data
+    public long getCount() {
+        return count;
+    }
+
+    public void setCount(long count) {
+        this.count = count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Country> getCountries() {
+        return countries;
+    }
+
+    public void setCountries(List<Country> countries) {
+        this.countries = countries;
+    }
+
     public static class Country {
 
         private String countryId;
@@ -22,6 +49,22 @@ public class Nationality {
 
         public Country(String countryId, float probability) {
             this.countryId = countryId;
+            this.probability = probability;
+        }
+
+        public String getCountryId() {
+            return countryId;
+        }
+
+        public void setCountryId(String countryId) {
+            this.countryId = countryId;
+        }
+
+        public float getProbability() {
+            return probability;
+        }
+
+        public void setProbability(float probability) {
             this.probability = probability;
         }
 
